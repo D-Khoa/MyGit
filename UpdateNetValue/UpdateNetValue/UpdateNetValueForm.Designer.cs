73f9542d46a41c625deacbee2b3a615c6472f760
DangKhoa <dk.wofts@gmail.com>
2019-11-01 16:54:46 +0700

    update net

---
+++ UpdateNetValue/UpdateNetValue/UpdateNetValueForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace UpdateNetValue
+{
+    partial class UpdateNetValueForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpdate = new Com.Nidec.Mes.Framework.ButtonCommon();
+            this.lbMonth = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.Control;
+            this.btnUpdate.ControlId = null;
+            this.btnUpdate.Font = new System.Drawing.Font("Arial", 9F);
+            this.btnUpdate.Location = new System.Drawing.Point(15, 39);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(128, 91);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // lbMonth
+            // 
+            this.lbMonth.AutoSize = true;
+            this.lbMonth.Location = new System.Drawing.Point(55, 9);
+            this.lbMonth.Name = "lbMonth";
+            this.lbMonth.Size = new System.Drawing.Size(58, 13);
+            this.lbMonth.TabIndex = 1;
+            this.lbMonth.Text = "MM/YYYY";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Month";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 404);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(659, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsStatus
+            // 
+            this.tsStatus.Name = "tsStatus";
+            this.tsStatus.Size = new System.Drawing.Size(644, 17);
+            this.tsStatus.Spring = true;
+            this.tsStatus.Text = "None";
+            // 
+            // dgvData
+            // 
+            this.dgvData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(0, 167);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(659, 234);
+            this.dgvData.TabIndex = 4;
+            // 
+            // UpdateNetValueForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 426);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbMonth);
+            this.Controls.Add(this.btnUpdate);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UpdateNetValueForm";
+            this.Text = "Update Net Value";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Com.Nidec.Mes.Framework.ButtonCommon btnUpdate;
+        private System.Windows.Forms.Label lbMonth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsStatus;
+        private System.Windows.Forms.DataGridView dgvData;
+    }
+}
+
---
+++ UpdateNetValue/UpdateNetValue/UpdateNetValueForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpdateNetValue
+{
+    public partial class UpdateNetValueForm : Form
+    {
+        TfSQL SQL;
+        DataTable dt, dt2;
+        StringBuilder cmd;
+        DateTime now;
+        DateTime start_depc, end_depc;
+        TimeSpan t;
+        double acq, monthly, current, accum, net;
+
+        public UpdateNetValueForm()
+        {
+            InitializeComponent();
+            SQL = new TfSQL();
+            dt = new DataTable();
+            dt2 = new DataTable();
+            cmd = new StringBuilder();
+        }
+
+        bool button = false;
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (!button)
+            {
+                button = true;
+                btnUpdate.Text = "Stop";
+                now = DateTime.Now;
+                //start_depc = new List<DateTime>();
+                //end_depc = new List<DateTime>();
+
+                cmd.Append("select a.asset_id, b.depreciation_start, b.depreciation_end, a.acquistion_cost, b.current_depreciation, b.monthly_depreciation, b.accum_depreciation_now, b.net_value ");
+                cmd.Append("from m_asset a");
+                cmd.Append(" left join t_account_main b on a.asset_id = b.asset_id");
+                SQL.sqlDataAdapterFillDatatable(cmd.ToString(), ref dt);
+                foreach (DataColumn c in dt.Columns)
+                {
+                    dt2.Columns.Add(c.ColumnName);
+                }
+                foreach (DataRow r in dt.Rows)
+                {
+                    start_depc = (DateTime)r["depreciation_start"];
+                    end_depc = (DateTime)r["depreciation_end"];
+                    acq = (double)r["acquistion_cost"];
+                    //t = end_depc - start_depc;
+                    //double diff = t.TotalDays / 30;
+                    if (end_depc.Month > start_depc.Month)
+                        double m = (double)((end_depc.Year - start_depc.Year) * 12 + (end_depc.Month - start_depc.Month));
+                    monthly = acq / (diff);
+                    t = now - start_depc;
+                    diff = t.TotalDays / 30;
+                    current = diff * monthly;
+                    accum = current + monthly;
+                    net = acq - current;
+                    dt2.Rows.Add(r["asset_id"], r["depreciation_start"], r["depreciation_end"], r["acquistion_cost"], current, monthly, accum, net);
+                }
+                dgvData.DataSource = dt2;
+            }
+            else
+            {
+                button = false;
+                btnUpdate.Text = "Update";
+            }
+        }
+    }
+}
